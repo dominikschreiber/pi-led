46784e2988b7d8b9aef4c6b92d60f2e3c04c6cdb
Dominik Schreiber <domischreib@gmail.com>
2014-06-28 17:37:25 +0200

    added express app, added /api/color/<hex-value> endpoint

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express')
+  , path = require('path')
+  , favicon = require('static-favicon')
+  , logger = require('morgan')
+  , cookieParser = require('cookie-parser')
+  , bodyParser = require('body-parser')
+  , routes = require('./routes/index')
+  , api = require('./routes/api')
+  , app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/api', api);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
---
+++ routes/api.js
@@ -0,0 +1,48 @@
+var router = require('express').Router()
+  , exec = require('child_process').exec
+  , params = require('express-params')
+  , pins = {
+        red: 18,
+        green: 23,
+        blue: 24
+    };
+
+params.extend(router);
+
+router.param('color', /^[0-9a-f]{6}$/);
+
+router.get('/color/:color', function(req, res) {
+    var components = req.params.color[0].match(/.{2}/g).map(function(c) { return parseInt(c, 16); })
+      , red = components[0]
+      , green = components[1]
+      , blue = components[2];
+
+    [{
+        pin: pins.red,
+        brightness: red
+    }, {
+        pin: pins.green,
+        brightness: green
+    }, {
+        pin: pins.blue,
+        brightness: blue
+    }].map(function(c) {
+        return {
+            pin: c.pin,
+            brightness: Math.round(c.brightness / 255.0, 2)
+        };
+    }).forEach(function(c) {
+        var command = 'echo "' + c.pin + '=' + c.brightness + '" > /dev/pi-blaster';
+
+        console.log(command);
+        exec(command);
+    });
+
+    res.json({
+        r: red,
+        g: green,
+        b: blue
+    });
+});
+
+module.exports = router;
