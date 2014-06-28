e68e4b0ac49699e137f874b7b0c97bc091906ed6
Dominik Schreiber <domischreib@gmail.com>
2014-06-28 22:42:21 +0200

    added saving last (currently active) color

--- routes/api.js
+++ routes/api.js
@@ -5,27 +5,37 @@ var router = require('express').Router()
         red: 18,
         green: 23,
         blue: 24
-    };
+    }
+  , currentColor = {};
 
 params.extend(router);
 
 router.param('color', /^[0-9a-f]{6}$/);
 
+router.get('/color', function(req, res) {
+    res.json({
+        r: currentColor.red || 0,
+        g: currentColor.green || 0,
+        b: currentColor.blue || 0
+    });
+});
+
 router.get('/color/:color', function(req, res) {
-    var components = req.params.color[0].match(/.{2}/g).map(function(c) { return parseInt(c, 16); })
-      , red = components[0]
-      , green = components[1]
-      , blue = components[2];
+    var components = req.params.color[0].match(/.{2}/g).map(function(c) { return parseInt(c, 16); });
+
+    currentColor.red = components[0];
+    currentColor.green = components[1];
+    currentColor.blue = components[2];
 
     [{
         pin: pins.red,
-        brightness: red
+        brightness: currentColor.red
     }, {
         pin: pins.green,
-        brightness: green
+        brightness: currentColor.green
     }, {
         pin: pins.blue,
-        brightness: blue
+        brightness: currentColor.blue
     }].map(function(c) {
         return {
             pin: c.pin,
